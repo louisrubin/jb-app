56f30fdea487cf26fd8cf34507d05b2d4ccd2f4a
louisrubin <miguel10.rbn@gmail.com>
2025-04-02 14:26:29 -0300

    fix responsive & orientation mode

--- src/components/ElEstudio.tsx
+++ src/components/ElEstudio.tsx
@@ -1,12 +1,63 @@
-import img1 from '../assets/jb-removebg2.png';
-// import img1 from '../assets/tatiana-pirelli.jpeg';
+import img1 from '../assets/joana-britez.jpeg';
 import img2 from '../assets/tatiana-pirelli.jpeg';
+// import img2 from '../assets/tatiana-pirelli-removebg.png';
 import './ElEstudio.css';
 import details from '../details';
+import { useEffect, useState } from 'react';
+
+
+
+// props como un objeto
+type props = { className?: string; };
+
+// desestructuración para extraer classNameParam
+const JB = ( { className = "" } : props ) => {
+    return(
+        <strong className={`strong-JB self-end text-right italic bg-[#cac4a2] ${className}`}
+        >JB Estudio Jurídico 
+            <span className="not-italic">⚖️</span>
+        </strong>
+    );
+}
 
 const ElEstudio = () => {
+    const [isMovile, setIsMovile] = useState(false);
+    const [modoHorizontal, setModoHorizontal] = useState(false);
+
+    // si volteo en modo horizontal
+    useEffect( () => {
+        const handleHorizontal = () => {
+            const modoHorizontal = window.matchMedia("(orientation: landscape)").matches;
+        
+            if (modoHorizontal) {
+                setModoHorizontal(true);    // set modo horizontal
+            } else {
+                setModoHorizontal(false);    // set modo horizontal
+            }
+        }
+        handleHorizontal();
+        
+        window.addEventListener("resize", handleHorizontal)
+        return () => window.removeEventListener("resize", handleHorizontal);
+    }, [] )
+
+    useEffect( () => {
+        const handleStrongJB = () => {
+            if (screen.height > screen.width) {
+                setIsMovile(true);    // es celular
+            } else {
+                setIsMovile(false);   // es monitor
+            }
+        }
+
+        handleStrongJB();   // al recargar la pagina
+        
+        window.addEventListener("resize", handleStrongJB)
+        return () => window.removeEventListener("resize", handleStrongJB);
+    }, [])
     return(
-        <div id="presentation-container" className="flex flex-col xl:flex-row justify-evenly gap-6 scroll-mt-45 mx-auto py-15 px-10 2xl:px-40 my-2 text-start bg-gray-300 rounded-xl">
+        <div id="presentation-container" className="flex flex-col xl:flex-row justify-center gap-6 scroll-mt-45 py-15 px-10 2xl:px-40 my-2 text-start bg-gray-300 rounded-xl">
+            
             {/* LADO IZQUIERDO */}
             <div className="estudio-left flex flex-col gap-8 md:w-[90%] lg:w-[75%] mx-auto xl:w-1/2 text-xl ">
             
@@ -16,20 +67,28 @@ const ElEstudio = () => {
                 
                 <p>Nos caracterizamos por brindar un asesoramiento y acompañamiento jurídico con responsabilidad, eficacia y seriedad.</p>
                 
-                <strong className={`strong-JB self-end text-right italic select-none bg-[#cac4a2]`}>JB Estudio Jurídico <span className="not-italic">⚖️</span></strong>
+                {   // componente sin parametros
+                    !isMovile ? <JB /> : null }
             </div>
 
+
             {/* LADO DERECHO */}
-            <div className="estudio-right flex flex-col xl:w-1/2 md:flex-row md:justify-evenly gap-3 text-xl rounded-xl">
+            <div className="estudio-right flex justify-center xl:w-2/5 gap-5 rounded-xl">
                 
                 { abogadasList.map( (item, index) => (
-                    <div key={index} className='relative md:w-1/2 max-h-[450px] max-w-[300px] mx-auto rounded-xl overflow-hidden'>
-                        <img src={ item.imagen } className='select-none w-full h-full object-cover'/>
-                        <p id='p-nombres-abogadas' className='absolute py-1 bottom-15 w-full text-center bg-[rgba(202,196,162,0.7)]'>{item.nombre}</p>
+                    <div key={index} className={`relative w-1/2 
+                        ${ modoHorizontal ? 'max-h-[25vw] max-w-[15vw] ' : 'max-h-[30vh] max-w-[20vh] ' }  md:mx-0 rounded-xl overflow-hidden`}>
+                        
+                        
+                        <img src={ item.imagen } className='select-none w-full h-full object-cover drop-shadow-[3px_3px_4px_rgba(0,0,0,1)] '/>
+                        <p id='p-nombres-abogadas' className='absolute py-1 bottom-5 w-full text-center bg-[rgba(202,196,162,1)]'>{item.nombre}</p>
                     </div>
                 ))}
 
             </div>
+
+            {   // componente con parametro
+                    isMovile ? <JB className='mt-10' /> : null }
         </div>
         
     );
@@ -39,12 +98,10 @@ const abogadasList = [
     {
         nombre: details.abogada1_name,
         imagen: img1,
-        styles: '',
     },
     {
         nombre: details.abogada2_name,
         imagen: img2,
-        styles: '',
     },
 ]
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,21 +5,48 @@ import logoHeader from "../assets/JB-light.png";
 import details from "../details";
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  // const [menuMovile, setMenuMovile] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [hidden, setHidden] = useState(false);
+    const [modoHorizontal, setModoHorizontal] = useState(false);
+    let lastScrollY = 0;
 
-  // const toggleMenu = () => setMenuMovile(!menuMovile); // invierte el boolean
+    // si volteo en modo horizontal el cel oculta el header al bajar
+    useEffect( () => {
+        const handleHorizontal = () => {
+            const scrollY = window.scrollY;
+            const modoHorizontal = window.matchMedia("(orientation: landscape)").matches;
+        
+            if (modoHorizontal && (screen.height < 720)) {
+                setModoHorizontal(true);    // set modo horizontal
 
-  useEffect(() => {
-    // maneja el scroll, si baja el header se achica
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+                if (scrollY > lastScrollY ) {
+                    setHidden(true);    // ocultar
+                } else {
+                    setHidden(false);   // mostrar al subir
+                }
+            } else {
+                setModoHorizontal(false);    // set modo horizontal
+                setHidden(false); // siempre visible
+            }
 
-    };
+            lastScrollY = scrollY;
+        }
+
+        handleHorizontal();
+
+        window.addEventListener("scroll", handleHorizontal);
+        return () => window.removeEventListener("scroll", handleHorizontal);
+    }, [] )
+    
+    useEffect(() => {
+        // verifica si bajó para modificar el Header
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -30,51 +57,69 @@ export default function Header() {
   const scrollToSection = (idParam: string) => {
     const section = document.getElementById(idParam);
     if (section) {
-      section.scrollIntoView( {behavior: "smooth", block: "start"} );
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
     }
-  }
+  };
 
   return (
     <header
       className={`fixed top-0 left-0 w-full transition-transform z-50 
-        text-center text-white pt-3 xl:px-[10%] shadow-lg
-        ${isScrolled 
-            ? "scrolled transform -translate-y-2 bg-gray-800 xl:flex justify-between " 
-            : ""}`}
+        text-center text-white pt-1 xl:px-[10%] shadow-lg
+        ${
+          isScrolled
+            ? "scrolled transform -translate-y-2 bg-gray-800 xl:flex justify-between "
+            : ""
+        }
+        ${
+            hidden ? "-translate-y-full" : "translate-y-0"
+        }`}
     >
-        <a id="a-direccion" target="_blank" rel="noopener noreferrer"
-            href={details.urlMap}
-            className={`text-gray-200 hover:underline 
-                        ${isScrolled ? "xl:hidden" : ""}`}
-        >   {details.direccion}
+      <a
+        id="a-direccion"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={details.urlMap}
+        className={`text-gray-200 hover:underline 
+                        ${isScrolled ? "xl:hidden" : ""}
+                        ${modoHorizontal ? "hidden" : ""}`}
+      >
+        {" "}
+        {details.direccion}
+      </a>
+
+      <div className="flex justify-center items-center select-none">
+        <a
+          onClick={() => {
+            scrollToSection(componentsNav[0].href_id);
+          }}
+          className="mr-1 md:mr-2 cursor-pointer"
+        >
+          <img src={logoHeader} className="w-20 h-20 xl:h-25 xl:w-25" />
         </a>
 
-        <div className="flex justify-center items-center select-none">
-            <a 
-                onClick={ () => { scrollToSection(componentsNav[0].href_id)} } 
-                className="mr-1 md:mr-2 cursor-pointer">
-                <img src={logoHeader} className="w-20 h-20 xl:h-25 xl:w-25" />
-            </a>
-
-            <div>
-                <h1 className={`h1-jb-estudio text-white text-2xl lg:text-3xl font-bold`}>
-                    {details.nombre}
-                </h1>
-                <p className="text-xs ">{details.descripcion}</p>
-            </div>
-            
+        <div>
+          <h1
+            className={`h1-jb-estudio text-white font-bold text-2xl lg:text-3xl`}
+          >
+            {details.nombre}
+          </h1>
+          <p className="text-xs ">{details.descripcion}</p>
         </div>
+      </div>
 
-        <div className={`hidden xl:flex justify-center items-center text-xs `}>
-            {componentsNav.map((item) => (
-            <a key={item.text}  onClick={ () => scrollToSection(item.href_id) }
-                className={`btn cursor-pointer px-3  ${ !isScrolled ? "py-3 " : "py-2" }`}>
-                {item.text}
-            </a>
-            ))}
-            
-        </div>
-
+      <div className={`hidden xl:flex justify-center items-center text-xs `}>
+        {componentsNav.map((item) => (
+          <a
+            key={item.text}
+            onClick={() => scrollToSection(item.href_id)}
+            className={`btn cursor-pointer px-3  ${
+              !isScrolled ? "py-3 " : "py-2"
+            }`}
+          >
+            {item.text}
+          </a>
+        ))}
+      </div>
     </header>
   );
 }
